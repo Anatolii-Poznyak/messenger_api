d93f5aa6ee8aa481ecfbfcbb51ecbf6280f5b9e2
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-22 22:50:01 +0300

    Add post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
