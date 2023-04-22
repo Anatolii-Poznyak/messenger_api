40b49b4dd1dbcde9cd7e24ca92534bdd3a2418cb
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-22 18:09:15 +0300

    add register user admin with username=email

--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
+    username = None
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
