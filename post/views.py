df5748232e1b294bfbb011b44b00b301038cb3c0
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-23 22:39:26 +0300

    Add IsAuthor permission

--- post/views.py
+++ post/views.py
@@ -1,15 +1,14 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 from post.models import Post
-from post.permissions import IsAdminOrIfAuthenticatedReadOnly
+from post.permissions import IsAdminOrIfAuthenticatedReadOnly, IsAuthorOrReadOnly
 from post.serializers import PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
