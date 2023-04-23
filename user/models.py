1bce6aea8504532d46fbb63fe582be10883554d9
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-24 00:08:51 +0300

    Fix names of file passes + add uploads dir to gitignore

--- user/models.py
+++ user/models.py
@@ -44,9 +44,9 @@ class UserManager(BaseUserManager):
 
 def user_picture_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+    filename = f"{slugify(instance.picture)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads/users/", filename)
 
 
 class User(AbstractUser):
