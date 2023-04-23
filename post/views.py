e493935138a94fbfb3ba0a616f0daa1dbd403500
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-23 23:02:09 +0300

    quick fix post_view permission_class variable

--- post/views.py
+++ post/views.py
@@ -9,7 +9,7 @@ from post.serializers import PostSerializer, PostDetailSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
