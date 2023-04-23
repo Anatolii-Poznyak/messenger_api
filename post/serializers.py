4de6cab33521d64c832fee6124f41ad8d3fbd553
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-23 22:58:46 +0300

    Add Post Detail Serializer with readonly fields

--- post/serializers.py
+++ post/serializers.py
@@ -9,3 +9,12 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ("id", "user", "content", "created_at")
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="user.full_name", read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ("id", "user", "content", "created_at")
+        read_only_fields = ("id", "user", "created_at")
