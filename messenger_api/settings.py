f0e5c346a1ce2ef1760c320e27810955eda96f6b
Anatolii Pozniak <a13101990a@gmail.com>
2023-04-24 01:54:29 +0300

    change throttling rates ( add a little bit)

--- messenger_api/settings.py
+++ messenger_api/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle"
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "10/minute", "user": "30/minute"},
+    "DEFAULT_THROTTLE_RATES": {"anon": "15/minute", "user": "40/minute"},
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
